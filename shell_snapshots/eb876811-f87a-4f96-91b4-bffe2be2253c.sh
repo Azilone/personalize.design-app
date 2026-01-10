79ee2b359707ea41a0e16dd3bbbd51997b038d59
Kevin <kevin.dejouet@gmail.com>
2026-01-10 09:55:20 +0100

    BMAD METHODE IA AGENT SETUP

---
+++ shell_snapshots/eb876811-f87a-4f96-91b4-bffe2be2253c.sh
@@ -0,0 +1,62 @@
+# Snapshot file
+# Unset all aliases to avoid conflicts with functions
+unalias -a 2>/dev/null || true
+# Functions
+
+# setopts 2
+setopt nohashdirs
+setopt login
+
+# aliases 2
+alias run-help=man
+alias which-command=whence
+
+# exports 48
+export B2_ACCOUNT_ID=0033e9e542a7c240000000003
+export B2_ACCOUNT_KEY=K003OUnyMQmY5BLGHOR+ZyynTaxX7Jo
+export CARAPACE_BRIDGES=zsh,fish,bash,inshellisense
+export CODEX_HOME='/Users/kevin/Documents/Code/personalize.design monorepo/personalize-design-app'
+export CODEX_MANAGED_BY_NPM=1
+export COLORTERM=truecolor
+export COMMAND_MODE=unix2003
+export -T FPATH fpath=( /opt/homebrew/share/zsh/site-functions /Users/kevin/.oh-my-zsh/plugins/z /Users/kevin/.oh-my-zsh/plugins/git /Users/kevin/.oh-my-zsh/functions /Users/kevin/.oh-my-zsh/completions /Users/kevin/.oh-my-zsh/custom/functions /Users/kevin/.oh-my-zsh/custom/completions /Users/kevin/.oh-my-zsh/cache/completions /Users/kevin/.zsh/completions /opt/homebrew/share/zsh/site-functions /usr/local/share/zsh/site-functions /usr/share/zsh/site-functions /usr/share/zsh/5.9/functions /Applications/OrbStack.app/Contents/MacOS/../Resources/completions/zsh /Applications/OrbStack.app/Contents/MacOS/../Resources/completions/zsh )
+export GHOSTTY_BIN_DIR=/Applications/Ghostty.app/Contents/MacOS
+export GHOSTTY_RESOURCES_DIR=/Applications/Ghostty.app/Contents/Resources/ghostty
+export GHOSTTY_SHELL_FEATURES=cursor,path,title
+export HOME=/Users/kevin
+export HOMEBREW_CELLAR=/opt/homebrew/Cellar
+export HOMEBREW_PREFIX=/opt/homebrew
+export HOMEBREW_REPOSITORY=/opt/homebrew
+export INFOPATH=/opt/homebrew/share/info:/opt/homebrew/share/info:
+export LANG=en_US.UTF-8
+export LESS=-R
+export LOGNAME=kevin
+export LSCOLORS=Gxfxcxdxbxegedabagacad
+export LS_COLORS='di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
+export LaunchInstanceID=F4DBE704-4D49-49B6-8289-224F3FE3132C
+export -T MANPATH manpath=( '' /usr/share/man /usr/local/share/man /Applications/Ghostty.app/Contents/Resources/ghostty/../man '' )
+export NODE_PATH=/Users/kevin/Library/pnpm/global/5/.pnpm/@openai+codex@0.79.0/node_modules/@openai/codex/bin/node_modules:/Users/kevin/Library/pnpm/global/5/.pnpm/@openai+codex@0.79.0/node_modules/@openai/codex/node_modules:/Users/kevin/Library/pnpm/global/5/.pnpm/@openai+codex@0.79.0/node_modules/@openai/node_modules:/Users/kevin/Library/pnpm/global/5/.pnpm/@openai+codex@0.79.0/node_modules:/Users/kevin/Library/pnpm/global/5/.pnpm/node_modules
+export OLDPWD=/Users/kevin
+export OSLogRateLimit=64
+export PAGER=less
+export -T PATH path=( /Users/kevin/.bun/bin /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin /System/Cryptexes/App/usr/bin /usr/bin /bin /usr/sbin /sbin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin '//Applications/Topaz Gigapixel AI.app/Contents/Resources/bin' /Library/Apple/usr/bin /var/folders/5y/t61sqzvj3pq9rjtqt91grykw0000gn/T/.tmpWLqtKj /Users/kevin/Library/pnpm/global/5/.pnpm/@openai+codex@0.79.0/node_modules/@openai/codex/vendor/aarch64-apple-darwin/path /Users/kevin/Library/pnpm /opt/homebrew/opt/libpq/bin /usr/local/opt/libpq/bin /Users/kevin/.antigravity/antigravity/bin /Users/kevin/.local/bin /Users/kevin/.codeium/windsurf/bin '/Users/kevin/Library/Application Support/carapace/bin' /Users/kevin/.deno/bin /Users/kevin/.bun/bin /Applications/Ghostty.app/Contents/MacOS /Users/kevin/.orbstack/bin /opt/homebrew/opt/fzf/bin )
+export PNPM_HOME=/Users/kevin/Library/pnpm
+export PWD=/Users/kevin
+export SECURITYSESSIONID=186b6
+export SHELL=/bin/zsh
+export -i10 SHLVL=2
+export SSH_AUTH_SOCK=/private/tmp/com.apple.launchd.GnYWAMeXTb/Listeners
+export STARSHIP_SESSION_KEY=2590158931413738
+export STARSHIP_SHELL=zsh
+export TERM=xterm-ghostty
+export TERMINFO=/Applications/Ghostty.app/Contents/Resources/terminfo
+export TERM_PROGRAM=ghostty
+export TERM_PROGRAM_VERSION=1.2.3
+export TMPDIR=/var/folders/5y/t61sqzvj3pq9rjtqt91grykw0000gn/T/
+export USER=kevin
+export XDG_DATA_DIRS=/usr/local/share:/usr/share:/Applications/Ghostty.app/Contents/Resources/ghostty/..
+export XPC_FLAGS=0x0
+export XPC_SERVICE_NAME=0
+export ZSH=/Users/kevin/.oh-my-zsh
+export __CFBundleIdentifier=com.mitchellh.ghostty
+export __CF_USER_TEXT_ENCODING=0x1F5:0x0:0x1
